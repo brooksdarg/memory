dda9c7db49ddbc28b826a0c3d072f31c5b34215a
Alex Levin <alexgalt@writeme.com>
2014-01-02 22:18:26 -0800

    commiting the memory game

---
+++ memory/views.py
@@ -0,0 +1,31 @@
+from pyramid.view import view_config
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
+from pprint import pprint
+import logging
+import random
+
+@view_config(route_name='home', renderer='templates/memory_home.pt')
+def home_view(request):
+    session = request.session
+    if 'grid' in session:
+       del session['grid']
+
+    return playing_view(request)     
+
+
+@view_config(route_name='playing', renderer='templates/memory_home.pt')
+def playing_view(request):
+       
+    session = request.session
+    if 'grid' in session:
+            grid = session['grid']
+            pprint( grid )
+            random.shuffle(grid)
+            pprint( grid )
+            session['grid'] = grid
+    else:
+            originals = random.sample(range(111), 16)
+            session['grid'] = originals
+
+    grid = session['grid']
+    return {'project': 'memory','grid': grid}
